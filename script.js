6b33533c21d2a04c187917da26d84be5473ed680
Rami Razaq <ramirazaq@gmail.com>
2024-08-09 17:51:14 -0500

    Added descriptions and updated the presentation layout

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function loadWebsite(url) {
-    const iframe = document.getElementById('website-frame');
-    iframe.src = url;
+function loadMainShortcut(url, description) {
+    document.getElementById('website-frame').src = url;
+    document.getElementById('main-description').textContent = description;
+    document.getElementById('sub-description').textContent = ''; // Clear sub-shortcut description
+}
+
+function loadSubShortcut(url, description) {
+    document.getElementById('website-frame').src = url;
+    document.getElementById('sub-description').textContent = description;
+    document.getElementById('main-description').textContent = ''; // Clear main shortcut description
 }
